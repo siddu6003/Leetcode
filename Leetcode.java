fd449eeffbeb42cc420699d4c4a30701ab91c39d
siddu6003 <51458421+siddu6003@users.noreply.github.com>
2022-05-09 15:47:43 +0530

    Arrays Linked Lists

--- Leetcode.java
+++ Leetcode.java
@@ -257,11 +257,272 @@ public List<Integer> majorityElement2(int[] nums) {
     return l;
 }
 
-
 // 62 . Unique Paths - pending
 
 // 493 . Reverse Pairs - pending
 
+// 1. Two Sum
+
+public int[] twoSum(int[] nums, int target) {
+    int[] result = new int[2];
+   Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+   for (int i = 0; i < nums.length; i++) {
+       if (map.containsKey(target - nums[i])) {
+           result[1] = i;
+           result[0] = map.get(target - nums[i]);
+           return result;
+       }
+       map.put(nums[i], i);
+   }
+   return result;
+}
+
+// 18 . 4 sum - pending
+
+// 128 . Longest Consecutive Sequence - pending
+
+// Largest subarray with 0 sum - pending
+
+// 3 . Longest Substring Without Repeating Characters  - pending
+
+
+
+// Linked List 
+
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+// 206 . Reverse Linked List
+
+// Using stack 
+
+public ListNode reverseList(ListNode head) {
+    Stack<ListNode> s=new Stack<>();
+    ListNode temp=head;
+    while(temp!=null){
+        s.push(temp);
+        temp=temp.next;
+    }
+    ListNode x=new ListNode(0);
+    ListNode p=x;
+    while(!s.isEmpty()){
+        p.next=s.pop();
+        p=p.next;
+    }
+    p.next=null;
+    return x.next;
+}
+
+// O(1) space
+
+public ListNode reverseList2(ListNode head) {
+    
+    ListNode l=head;
+    ListNode prev=null;
+    while(l!=null){
+     ListNode nextTemp = l.next;
+        l.next=prev;
+        prev=l;
+        l=nextTemp;
+       
+    }
+     return prev;
+}
+
+// 876 . Middle of the Linked List
+
+public ListNode middleNode(ListNode head) {
+    ListNode slow=head;
+    ListNode fast=head;
+    while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return slow;
+}
+
+// 21 . Merge Two Sorted Lists
+
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode temp=new ListNode(0);
+    ListNode x=temp;
+    ListNode head1=l1;
+    ListNode head2=l2;
+    while(head1!=null && head2!=null){
+        if(head1.val>head2.val){
+            temp.next=head2;
+            temp=temp.next;
+            head2=head2.next;
+        }else{
+            temp.next=head1;
+            temp=temp.next;
+            head1=head1.next;
+        }
+    }
+    
+    while(head1!=null){
+        temp.next=head1;
+        temp=temp.next;
+        head1=head1.next;
+    }
+    
+     while(head2!=null){
+        temp.next=head2;
+        temp=temp.next;
+        head2=head2.next;
+    }
+    
+    return x.next;
+}
+
+// 19 . Remove Nth Node From End of List
+
+public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode start = new ListNode();
+     start.next = head;
+     ListNode fast = start;
+     ListNode slow = start;     
+
+     for(int i = 1; i <= n; ++i)
+         fast = fast.next;
+ 
+     while(fast.next != null)
+     {
+         fast = fast.next;
+         slow = slow.next;
+     }
+     
+     slow.next = slow.next.next;
+     
+     return start.next;
+ }
+
+ // 2. Add Two Numbers
+
+ public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = l1, q = l2, curr = dummyHead;
+    int carry = 0;
+    while (p != null || q != null) {
+        int x = (p != null) ? p.val : 0;
+        int y = (q != null) ? q.val : 0;
+        int sum = carry + x + y;
+        carry = sum / 10;
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        if (p != null) p = p.next;
+        if (q != null) q = q.next;
+    }
+    if (carry > 0) {
+        curr.next = new ListNode(carry);
+    }
+    return dummyHead.next;
+}
+
+// 237 . Delete Node in a Linked List
+
+public void deleteNode(ListNode node) {
+    node.val=node.next.val;
+    node.next=node.next.next;
+}
+
+
+// 160 . Intersection of Two Linked Lists
+
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode tem1=headA;
+    ListNode tem2=headB;
+    int s1=0;
+    int s2=0;
+    while(tem1!=null){
+        s1++;
+        tem1=tem1.next;
+    }
+    while(tem2!=null){
+        s2++;
+        tem2=tem2.next;
+    }
+    int diff=Math.abs(s1-s2);
+    
+    for(int i=0;i<diff;i++){
+        if(s1>s2){
+            headA=headA.next;
+        }else{
+            headB=headB.next;
+        }
+    }
+    
+    while(headA!=headB){
+        headA=headA.next;
+        headB=headB.next;
+    }
+    return headA;
+}
+
+// 141 . Linked List Cycle
+
+public boolean hasCycle(ListNode head) {
+    if(head==null || head.next==null) return false;
+    ListNode slow=head;
+    ListNode fast=head;
+    while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+        if(fast==slow){
+            return true;
+        }
+    }
+    return false;
+}
+
+// 25 . Reverse Nodes in k-Group - pending
+
+// 234 . Palindrome Linked List - pending
+
+public boolean isPalindrome(ListNode head) {
+    Stack<Integer> s=new Stack<>();
+    ListNode temp=head;
+    while(temp!=null){
+        s.push(temp.val);
+        temp=temp.next;
+    }
+    while(!s.isEmpty()){
+        int k=s.pop();
+        if(head.val!=k){
+            return false;
+        }
+        head=head.next;
+    }
+    return true;
+}
+
+// 142 . Linked List Cycle II
+
+public ListNode detectCycle(ListNode head) {
+    if(head==null || head.next==null) return null;
+    ListNode slow=head;
+    ListNode fast=head;
+    ListNode x=head;
+    while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+        if(fast==slow){
+             while(slow!=x){
+                x = x.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+ return null;
+}
+
+// Day 7 pending 
 
 
 
